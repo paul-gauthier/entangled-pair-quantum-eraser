89e290323a1d599fb93a6a7a41efe7843401be8b
Paul Gauthier <paul@aider.chat>
2025-05-27 17:06:02 -0700

    chore: set epsilon values to 0

--- lab6entangled.py
+++ lab6entangled.py
@@ -397,8 +397,8 @@ assert phi_plus_state.norm() == 1
 ##############################################################
 
 
-idler_epsilon = math.radians(5)
-signal_epsilon = math.radians(5)
+idler_epsilon = math.radians(0)
+signal_epsilon = math.radians(0)
 
 
 
